2812ae775328e5cf3d2de96512a97cd3ab0540f8
Pokejoueur <106403938+Pokejoueur@users.noreply.github.com>
2025-05-30 17:43:07 +0200

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import alpinejs from '@astrojs/alpinejs';
+import node from '@astrojs/node';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
+  integrations: [
+    tailwind(),
+    alpinejs(),
+  ],
+});
