0a40ed06a20a8000de3591107a562542f704619c
Pokejoueur <106403938+Pokejoueur@users.noreply.github.com>
2025-05-30 12:15:48 +0200

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        fandomBlue: '#0055AA',
+        fandomGray: '#F8F9FA',
+        fandomDarkGray: '#2C2F33'
+      }
+    },
+  },
+  plugins: [],
+};
